58fd89018910836f466d8bb46960f3d76e298fe6
“Stanislav <gorokht6mail.ru@gmail.com>
2024-02-10 14:40:04 +0200

    feat: create funtion to get user by id and throw an error if somwthing wrong

--- src/modules/getUserByID.js
+++ src/modules/getUserByID.js
@@ -4,8 +4,32 @@ const getUserID = async (USESR_DB, url, res) => {
   getUserByID(USESR_DB, id, res)
 };
 
+const isUUID = async (id) => {
+  const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  // return uuidPattern.test(id);
+  return true;
+};
+
 const getUserByID = async (USESR_DB, id, res) => {
+  let statusCode = 200;
+  let responseBody = null;
+
   const user = await USESR_DB.filter((e) => e.id === Number(id));
+  if(await isUUID(id) === false) {
+    statusCode = 400;
+    responseBody = {
+      message: "userId is invalid (not uuid)"
+    };
+  } else if (!user) {
+    statusCode = 404;
+    responseBody = {
+      message: "user with id === userId doesn't exist"
+    };
+  } else {
+    responseBody = user;
+  }
+  res.writeHead(statusCode);
+  res.end(JSON.stringify(responseBody));
 };
 
 export { getUserID };
