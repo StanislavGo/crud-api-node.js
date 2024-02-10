dedb9229832cd52431731b5e4128f036c73a9fc7
“Stanislav <gorokht6mail.ru@gmail.com>
2024-02-10 17:07:26 +0200

    fix: fix problem with setting headers while POSTing user

--- src/modules/postUser.js
+++ src/modules/postUser.js
@@ -9,28 +9,29 @@ const postUser = async (USERS_DB, req, res) => {
 
 
     req.on("end", () => {
+      let status = 201;
       const userData = JSON.parse(requestBody);
-
-      if(!userData.username || !userData.age || !userData.hobbies) {
-        res.writeHead(400);
-        res.end(JSON.stringify({ message: "request body does not contain required fields" }));
-      }
-  
       const id = uuid4();
-  
+
       const newUser = {
         id,
         username: userData.username,
         age: userData.age,
         hobbies: userData.hobbies,
       }
+
+      let body = JSON.stringify(newUser, null, 4);
+
+      if(!userData.username || !userData.age || !userData.hobbies) {
+        status = 400;
+        body = JSON.stringify({ message: "request body does not contain required fields" });
+      }
   
       USERS_DB.push(newUser);
   
-      res.writeHead(201);
-      res.end(JSON.stringify(newUser, null, 4));
+      res.writeHead(status);
+      res.end(body);
     });
-    console.log(USERS_DB);
   } catch {
     res.writeHead(500);
     res.end(JSON.stringify({ message: 'Internal Server Error' }));
